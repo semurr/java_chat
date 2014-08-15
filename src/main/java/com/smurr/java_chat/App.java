3be411cfc681a34141e04b910f5feed88ee7b918
Stephen Murray <smurray@live.com>
2014-08-14 22:21:50 -0700

    init checkin

---
+++ src/main/java/com/smurr/java_chat/App.java
@@ -0,0 +1,13 @@
+package com.smurr.java_chat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
